c0bd5e998a4180b7b13b8d635f5ef5455eda9ee4
y.tsokota <y.tsokota@gmail.com>
2019-05-12 19:32:36 +0300

    Add Animal products and update old

---
+++ FarmApp/AnimalProduct.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmApp
+{
+    public class AnimalProduct
+    {
+        public string Name { get; }
+        public decimal UnitPrice { get; set; }
+
+        public AnimalProduct(string name, decimal unitPrice)
+        {
+            Name = name;
+            UnitPrice = unitPrice;
+        }
+    }
+
+    public class CowsProductMilk : AnimalProduct
+    {
+        public CowsProductMilk(decimal unitPrice) : base("Milk from cows", unitPrice) { }
+    }
+
+    public class HensProductEggs : AnimalProduct
+    {
+        public HensProductEggs(decimal unitPrice) : base("Eggs from hens", unitPrice) { }
+    }
+
+    public class SheepsProductWool : AnimalProduct
+    {
+        public SheepsProductWool(decimal unitPrice) : base("Sheep from wool", unitPrice) { }
+    }
+}
---
+++ FarmApp/AnimalProductsEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmApp
+{
+    public enum AnimalProductsEnum : byte
+    {
+        Eggs = 1,
+        Milk,
+        Wool,
+        Potato
+    }
+}
